b4f202f1aed2c49329aea77173fc3ffd48c6d414
KIM JIWON <168164096+kwldnjs1215@users.noreply.github.com>
2024-05-23 14:21:05 +0900

    Create ADHD_database.sql

---
+++ ADHD/03_Flask/ADHD_database.sql
@@ -0,0 +1,26 @@
+DROP TABLE adhd_table;
+
+CREATE TABLE adhd_table (
+    Q1 INT,
+    Q2 INT,
+    Q3 INT,
+    Q4 INT,
+    Q5 INT,
+    Q6 INT,
+    Q7 INT,
+    Q8 INT,
+    Q9 INT,
+    Q10 INT,
+    Q11 INT,
+    Q12 INT,
+    Q13 INT,
+    Q14 INT,
+    Q15 INT,
+    Q16 INT,
+    Q17 INT,
+    Q18 INT
+);
+
+COMMIT;
+
+SELECT * FROM adhd_table;
