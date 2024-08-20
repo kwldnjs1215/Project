2fb45a9f46895ba1e97b3905537a7256d2cecc6a
KIM JIWON <168164096+kwldnjs1215@users.noreply.github.com>
2024-08-20 15:22:27 +0900

    Update amenities_count_code.py

--- ArtMuseum/amenities_count_code.py
+++ ArtMuseum/amenities_count_code.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import requests
 
-# Function to get all elements with tags using Overpass API
+# Overpass API를 사용하여 태그가 있는 모든 요소를 가져오는 함수
 def get_all_tags_osm(latitude, longitude, radius=1000):  # 1km 반경
     overpass_url = "http://overpass-api.de/api/interpreter"
     overpass_query = f"""
@@ -19,19 +19,19 @@ def get_all_tags_osm(latitude, longitude, radius=1000):  # 1km 반경
 
 file_name = r'C:\Users\User\Desktop\02_공모전\6월_2024_문화_디지털혁신_및_문화데이터_활용_공모전\museum_data\museum_list_data.csv'
 
-# Load the CSV file with the detected encoding
+# 감지된 인코딩으로 CSV 파일을 로드
 museum_data = pd.read_csv(file_name, encoding='utf-8')
 
-# Dictionary to hold all amenity tag counts
+# 모든 amenity 태그의 개수를 저장할 딕셔너리
 all_amenity_counts = {index: {} for index in museum_data.index}
 
-# Search for all tags for each museum
+# 각 박물관에 대한 모든 태그 검색
 for index, row in museum_data.iterrows():
     latitude = row['FCLTY_LA']
     longitude = row['FCLTY_LO']
     nearby_places = get_all_tags_osm(latitude, longitude)
 
-    # Count the frequency of each amenity subcategory
+    # 각 amenity 하위 카테고리의 빈도를 계산
     amenity_counts = all_amenity_counts[index]
     for place in nearby_places:
         if 'tags' in place and 'amenity' in place['tags']:
@@ -41,18 +41,18 @@ for index, row in museum_data.iterrows():
             else:
                 amenity_counts[amenity] = 1
 
-# Get all unique amenity subcategories
+# 모든 고유한 amenity 하위 카테고리를 가져옴
 all_amenities = set(amenity for amenity_counts in all_amenity_counts.values() for amenity in amenity_counts)
 
-# Add columns for all amenity subcategories and set initial values to 0
+# 모든 amenity 하위 카테고리에 대한 열을 추가하고 초기값을 0으로 설정
 for amenity in all_amenities:
     museum_data[f'amenity.{amenity}'] = 0
 
-# Fill in the counts
+# 개수 채우기
 for index, amenity_counts in all_amenity_counts.items():
     for amenity, count in amenity_counts.items():
         museum_data.at[index, f'amenity.{amenity}'] = count
 
-# Save csv with utf-8 encoding
+# utf-8 인코딩으로 CSV 파일 저장
 output_file_name = r'C:\Users\User\Desktop\02_공모전\6월_2024_문화_디지털혁신_및_문화데이터_활용_공모전\overpass_api_map\all\national_art_museum_with_amenity_counts.csv'
 museum_data.to_csv(output_file_name, index=False, encoding='euc-kr')
